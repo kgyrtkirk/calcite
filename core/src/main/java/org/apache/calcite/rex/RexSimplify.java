17118563a342a4144843a0d6df7b58cc790a57cb
Zoltan Haindrich <kirk@rxd.hu>
2018-10-16 16:54:42 +0200

    damn whitespace

--- core/src/main/java/org/apache/calcite/rex/RexSimplify.java
+++ core/src/main/java/org/apache/calcite/rex/RexSimplify.java
@@ -51,6 +51,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
+
 import static org.apache.calcite.rex.RexUnknownAs.FALSE;
 import static org.apache.calcite.rex.RexUnknownAs.UNKNOWN;
 
