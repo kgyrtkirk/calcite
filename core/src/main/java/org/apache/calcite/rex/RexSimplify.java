af6da12efe7d9b661cd238ce7b44664df4c7b86d
Zoltan Haindrich <kirk@rxd.hu>
2019-03-01 16:48:53 +0100

    no-more backstabbing dx

--- core/src/main/java/org/apache/calcite/rex/RexSimplify.java
+++ core/src/main/java/org/apache/calcite/rex/RexSimplify.java
@@ -2160,7 +2160,8 @@ public class RexSimplify {
   public RexNode simplifyFilterPredicates(Iterable<? extends RexNode> predicates) {
     final RexNode simplifiedAnds =
         withPredicateElimination(Bug.CALCITE_2401_FIXED)
-            .simplifyAnds(predicates, FALSE);
+            .simplifyUnknownAsFalse(
+                RexUtil.composeConjunction(rexBuilder, predicates));
     if (simplifiedAnds.isAlwaysFalse()) {
       return null;
     }
