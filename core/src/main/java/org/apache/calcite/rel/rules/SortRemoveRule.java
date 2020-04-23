2beeeed7de7420c1007b0b9c9b302e9e221a527d
Vineet Garg <vgarg@apache.org>
2020-04-23 15:46:06 -0700

    [CALCITE-3952] Improve SortRemoveRule to remove Sort based on rowcount (Vineet Garg)

--- core/src/main/java/org/apache/calcite/rel/rules/SortRemoveRule.java
+++ core/src/main/java/org/apache/calcite/rel/rules/SortRemoveRule.java
@@ -21,8 +21,11 @@ import org.apache.calcite.plan.RelOptRuleCall;
 import org.apache.calcite.plan.RelTraitSet;
 import org.apache.calcite.rel.RelCollation;
 import org.apache.calcite.rel.RelCollationTraitDef;
+import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.RelFactories;
 import org.apache.calcite.rel.core.Sort;
+import org.apache.calcite.rel.metadata.RelMetadataQuery;
+import org.apache.calcite.rex.RexLiteral;
 import org.apache.calcite.tools.RelBuilderFactory;
 
 /**
@@ -45,15 +48,32 @@ public class SortRemoveRule extends RelOptRule {
   }
 
   @Override public void onMatch(RelOptRuleCall call) {
+    boolean sortRemoved = removeSortIfInputAlreadySorted(call);
+    if (!sortRemoved) {
+      // try row count based removal
+      final Sort sort = call.rel(0);
+      int fetch = (sort.getFetch() != null) && (sort.getFetch() instanceof RexLiteral)
+          ? RexLiteral.intValue(sort.getFetch()) : -1;
+      int offset = (sort.getOffset() != null) && (sort.getOffset() instanceof RexLiteral)
+          ? RexLiteral.intValue(sort.getOffset()) : -1;
+
+      if (shouldRemoveSortBasedOnRowCount(call.getMetadataQuery(),
+          sort.getInput(), fetch, offset)) {
+        call.transformTo(sort.getInput());
+      }
+    }
+  }
+
+  protected  boolean removeSortIfInputAlreadySorted(RelOptRuleCall call) {
     if (!call.getPlanner().getRelTraitDefs()
         .contains(RelCollationTraitDef.INSTANCE)) {
       // Collation is not an active trait.
-      return;
+      return false;
     }
     final Sort sort = call.rel(0);
     if (sort.offset != null || sort.fetch != null) {
       // Don't remove sort if would also remove OFFSET or LIMIT.
-      return;
+      return false;
     }
     // Express the "sortedness" requirement in terms of a collation trait and
     // we can get rid of the sort. This allows us to use rels that just happen
@@ -63,5 +83,27 @@ public class SortRemoveRule extends RelOptRule {
         .getTrait(RelCollationTraitDef.INSTANCE);
     final RelTraitSet traits = sort.getInput().getTraitSet().replace(collation);
     call.transformTo(convert(sort.getInput(), traits));
+    return true;
+  }
+
+  // Sort is removed if all of following conditions are met
+  // 1. Sort's input is guaranteed to produce at most one row
+  // 2. If fetch is greater than 0
+  // 3. If offset is less than 1
+  public static boolean shouldRemoveSortBasedOnRowCount(final RelMetadataQuery mq,
+      final RelNode input, int fetch, int offset) {
+    Double maxRowCount = mq.getMaxRowCount(input);
+    if (maxRowCount != null && maxRowCount <= 1) {
+      if (fetch == 0) {
+        return false;
+      } else if (fetch > 0) {
+        if (offset < 1) {
+          return true;
+        }
+      } else {
+        return true;
+      }
+    }
+    return false;
   }
 }
